37223a00ed32aea7d245c52f6486d983c0a69d08
Tim Popov <popov_ta@mail.ru>
2020-07-17 13:43:10 +0300

    Что-то первое

---
+++ MyMobile/MyMobile/DbProxy.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MyMobile
+{
+    public class DbProxy
+    {
+        public static List<Avtomat> Avtomats;
+        public static List<Ingredient> Ingridients;
+        public static void SaveData()
+        {
+        }
+
+        public static void LoadData()
+        {
+            Avtomats = new List<Avtomat>();
+            Ingridients = new List<Ingredient>();
+            TestData();
+        }
+
+
+        static void TestData()
+        {
+            Avtomats.Add(new Avtomat() {Value = "Хлебозавод"});
+            Avtomats.Add(new Avtomat() {Value = "Мираторг строитель"});
+            Avtomats.Add(new Avtomat() {Value = "Площадь Василевского"});
+            Avtomats.Add(new Avtomat() {Value = "Дом профсоюзов"});
+            Avtomats.Add(new Avtomat() {Value = "Бау Мосиев"});
+            Avtomats.Add(new Avtomat() {Value = "БМК"});
+            Avtomats.Add(new Avtomat() {Value = "Мираторг лев"});
+
+            Ingridients.Add(new Ingredient(){Value = "Стаканы"});
+            Ingridients.Add(new Ingredient() { Value = "Палочки" });
+            Ingridients.Add(new Ingredient() { Value = "Сахар" });
+            Ingridients.Add(new Ingredient() { Value = "Вода" });
+            Ingridients.Add(new Ingredient() { Value = "Коф.зерн" });
+            Ingridients.Add(new Ingredient() { Value = "Сливки" });
+            Ingridients.Add(new Ingredient() { Value = "Чай" });
+        }
+    }
+
+    public class Avtomat
+    {
+        public Avtomat()
+        {
+            Id=Guid.NewGuid();
+        }
+
+
+        public Guid Id { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public Ingredient()
+        {
+            Id=Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    public class Record
+    {
+        /// <summary>
+        /// Какой автомат
+        /// </summary>
+        public Guid Avtomat { get; set; }
+        /// <summary>
+        /// Какой ингредиент
+        /// </summary>
+        public Guid Ingredient { get; set; }
+        /// <summary>
+        /// Значение
+        /// </summary>
+        public double Value { get; set; }
+
+        /// <summary>
+        /// Дата установки
+        /// </summary>
+        public string Date { get; set; }
+    }
+
+}
--- MyMobile/MyMobile/MainPage.xaml.cs
+++ MyMobile/MyMobile/MainPage.xaml.cs
@@ -16,6 +16,34 @@ namespace MyMobile
         public MainPage()
         {
             InitializeComponent();
+            DbProxy.LoadData();
+            InitializePage();
+        }
+
+        void InitializePage()
+        {
+            List<string> Items = DbProxy.Ingridients.Select(c => c.Value).ToList();
+
+            Button avtomatButton = new Button() { Text = "Автоматы" };
+            Grid.SetColumn(avtomatButton,0);
+
+            Button ingredientButton=new Button(){Text = "Ингредиенты"};
+            Grid.SetColumn(ingredientButton,1);
+
+            ListView ingridienView = new ListView() {ItemsSource = Items};
+            Grid.SetRow(ingridienView,0);
+
+            Grid buttonGrid = new Grid()
+            {
+                ColumnDefinitions = new ColumnDefinitionCollection() {new ColumnDefinition(), new ColumnDefinition()},
+                Children = {avtomatButton, ingredientButton}
+            };
+
+            Grid.SetRow(buttonGrid,1);
+
+            Grid mainGrid=new Grid(){RowDefinitions = new RowDefinitionCollection(){new RowDefinition(),new RowDefinition(){Height = GridLength.Auto}},Children = {ingridienView,buttonGrid }};
+
+            this.Content = new StackLayout() {Children = {mainGrid}};
         }
     }
 }
