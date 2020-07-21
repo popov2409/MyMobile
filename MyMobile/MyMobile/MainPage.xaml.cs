67abef5e828a76cb48d09f7cdd9fb952d610e0fd
Tim Popov <popov_ta@mail.ru>
2020-07-22 00:47:49 +0300

    Все кроме сохранения данных

--- MyMobile/MyMobile/MainPage.xaml.cs
+++ MyMobile/MyMobile/MainPage.xaml.cs
@@ -18,40 +18,74 @@ namespace MyMobile
             InitializeComponent();
             DbProxy.LoadData();
             InitializePage();
+            StartPages();
+           
+        }
+
+
+        void StartPages()
+        {
+            AvtomatGrid.IsVisible = true;
+            InputDataGrid.IsVisible = false;
         }
 
         void InitializePage()
         {
-            IngredientListView.ItemsSource = DbProxy.Ingridients.OrderBy(c=>c.Value);
+            IngredientListView.ItemsSource = DbProxy.Ingridients.OrderBy(c => c.Value);
             AvtomatListView.ItemsSource = DbProxy.Avtomats.OrderBy(c => c.Value);
         }
 
 
-        private void AvtomatButton_OnClicked(object sender, EventArgs e)
-        {
-            AvtomatGrid.IsVisible = true;
-            IngredienGrid.IsVisible = false;
-            AvtomatButton.BackgroundColor = Color.DarkOrange;
-            IngredientButton.BackgroundColor = Color.Black;
-            AvtomatButton.FontSize = 18;
-            IngredientButton.FontSize = 16;
-        }
+        //private void AvtomatButton_OnClicked(object sender, EventArgs e)
+        //{
+        //    AvtomatGrid.IsVisible = true;
+        //    IngredienGrid.IsVisible = false;
+        //    AvtomatButton.BackgroundColor = Color.DarkOrange;
+        //    IngredientButton.BackgroundColor = Color.Black;
+        //    AvtomatButton.FontSize = 18;
+        //    IngredientButton.FontSize = 16;
+        //}
 
-        private void IngredientButton_OnClicked(object sender, EventArgs e)
-        {
-            AvtomatGrid.IsVisible = false;
-            IngredienGrid.IsVisible = true;
+        //private void IngredientButton_OnClicked(object sender, EventArgs e)
+        //{
+        //    AvtomatGrid.IsVisible = false;
+        //    IngredienGrid.IsVisible = true;
 
-            AvtomatButton.BackgroundColor = Color.Black;
-            IngredientButton.BackgroundColor = Color.DarkOrange;
+        //    AvtomatButton.BackgroundColor = Color.Black;
+        //    IngredientButton.BackgroundColor = Color.DarkOrange;
 
-            AvtomatButton.FontSize = 16;
-            IngredientButton.FontSize = 18;
-        }
+        //    AvtomatButton.FontSize = 16;
+        //    IngredientButton.FontSize = 18;
+        //}
 
         private void AvtomatSearchEntry_OnTextChanged(object sender, TextChangedEventArgs e)
         {
             AvtomatListView.ItemsSource = DbProxy.Avtomats.Where(c=>c.Value.ToLower().Contains(AvtomatSearchEntry.Text.ToLower())).OrderBy(c => c.Value);
         }
+
+        private void InputDataButton_OnClicked(object sender, EventArgs e)
+        {
+            if (AvtomatListView.SelectedItem == null)
+            {
+                DisplayAlert("Уведомление", "Не выбран автомат!", "OK");
+                return;
+            }
+
+            AvtomatGrid.IsVisible = false;
+            InputDataGrid.IsVisible = true;
+            Avtomat a=AvtomatListView.SelectedItem as Avtomat;
+            AvtomatNameLabel.Text = a.Value;
+        }
+
+        private void BackButton_OnClicked(object sender, EventArgs e)
+        {
+            StartPages();
+        }
+
+        private void VisualElement_OnUnfocused(object sender, FocusEventArgs e)
+        {
+            if ((sender as Entry).Text.Trim().Length == 0) (sender as Entry).Text = "0";
+        }
+
     }
 }
